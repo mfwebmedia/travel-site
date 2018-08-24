b56e76b4f3b3fdd5287b943483d4c2c6211ec22c
Mark <kontakt@markfricke.de>
2018-08-25 00:47:50 +0200

    Lesson 12 done

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp'),
+watch = require('gulp-watch'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested');
+
+gulp.task('default', function() {
+    console.log("Hooray - dein Gulp task läuft!");
+});
+
+gulp.task('html', function() {
+    console.log("Stell dir was Sinnvolles vor was hier mit deinem HTML gemacht wird.");
+});
+
+gulp.task('styles', function() {
+    return gulp.src('./app/assets/styles/styles.css')
+        .pipe(postcss([cssvars, nested, autoprefixer]))
+        .pipe(gulp.dest('./app/temp/styles'));
+});
+
+gulp.task('watch', function() {
+    watch('./app/index.html', function() {
+        gulp.start('html');
+    });
+
+    watch('./app/assets/styles/**/*.css', function() {
+        gulp.start('styles');
+    })
+});
+
